51a13219073c3164a5e985d473ac5f8572ef315a
Tunahan Tatli <tunahantatl@gmail.com>
2022-12-13 11:43:10 +0300

    cmmt-variables

---
+++ pythonTraining/intro.py
@@ -0,0 +1,54 @@
+#Run below code
+print (3+3)
+print (type(3.7))
+print (type(3))
+print (type(3.0))
+print (type(3.0+4))
+print (type(4.2+3))
+print (type(2.0+3.0))
+print (type(3.2-0.2))
+
+# 6 = int type
+# 6.0, 6.1, 6.2 = float 
+
+# Also How we do Comment in Python?
+# if we will single line comment we can use #
+""" but we want to multi-line comment we can use(''')"""
+
+"""if you using VS Code and if you want to convert to comment a part of the code
+you can use ctrl+k+c shortcut after select to part.
+convert code2comment= ctrl+k+c
+convert comment2code= ctrl+k+u
+"""
+
+
+# ***String*** 
+
+a = 4 #  type a int
+b = "4" # type b ?
+# let print they
+print(type(a)) # type a int
+print(type(b)) # type b string
+
+# How we to identify variables name in Python?
+
+"""
+example:
+studentSchoolNumber = "362" -> this wrong
+studentSnumber = "362" -> this wrong
+student_num = "362" -> this is correct
+school_num = "362" -> this is correct
+  variables name should be lowercase, short, understandable
+  if variables name consists of more than one word, should use underdash
+  !!! important 1(one) l(L) O(o uppercase letter) should'nt use :)
+         examle;
+           1examle (starting number 1)
+           lexamle  (starting letter L)
+           0exam (starting number 0)
+           Oexam (starting letter o)
+"""
+# let we do an example 
+banana_price = 2
+total_amount_bought = 5
+print(banana_price * total_amount_bought)
+# answer is 10
